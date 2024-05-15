062e93fff498a2195a3e03c59474b121c16f581c
Expasil <arsenij08022001@gmail.com>
2024-05-16 00:26:16 +0500

    booking and menu models added

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class booking(models.Model):
+    Name = models.CharField(max_length=255)
+    No_of_guests = models.IntegerField(max_length=6)
+    BookingDate = models.DateTimeField()
+
+
+class menu(models.Model):
+    Title = models.CharField( max_length=255 )
+    Price = models.DecimalField(decimal_places=2, max_digits=10)
+    Inventory = models.IntegerField(max_length=5)
+
+    
